28af3d83e11624d4eb96f564e46b53bd74b1aedf
BruhSlave <braslpaf@gmail.com>
2026-02-20 22:10:55 +0300

    Add in V002 migration - add foreign key

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -7,5 +7,9 @@ ADD COLUMN price DOUBLE PRECISION;
 ALTER TABLE orders  
 ADD COLUMN date_created DATE;
 
-
+ALTER TABLE order_product
+ADD CONSTRAINT fk_order_product_product FOREIGN KEY (product_id)
+  REFERENCES product(id),
+ADD CONSTRAINT fk_order_product_orders FOREIGN KEY (order_id)
+  REFERENCES orders(id);
 
